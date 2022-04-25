7de9a0809aaea7fb49f9c34db39cd55043519baa
phyo <ayehtet4198@gmail.com>
2022-04-25 13:17:52 +0630

    プロジェクト ファイルを追加します。

---
+++ Koutei/UserControl/UC01board.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Koutei.UserControl
+{
+    public partial class UC01board : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region "保留ボードに付箋データ設定"
+
+        /// <summary>
+        /// 保留ボードに付箋データ設定
+        /// </summary>
+        /// <param name="dtFusen"></param>
+        public void SetPendingFusenBoardData()
+        {
+            pnlFusen.CssClass = "UC02FusenInfoDiv PendingBoardDiv";
+            divPendingHeader.Style.Add("display", "block");
+            lblPendingHeader.Text = Session["BoardName"].ToString();
+            lblPendingHeader_ID.Text = Session["BoardID"].ToString();
+            
+            divFusenList.Style.Add("background-color", "rgb(197,224,245)");
+        }
+
+        #endregion
+
+    }
+}
